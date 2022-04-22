e2a9b97237dc197ae5bff885ce3e24e30b4ead69
Your Name <you@example.com>
2022-04-22 14:59:16 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -97,7 +97,7 @@ TreeNode * minimum(TreeNode * x){
 
 void removeNode(TreeMap * tree, TreeNode* node) {
 
-    if ((node->left == NULL) && (node->right) == NULL)
+    if ((node->left == NULL) && (node->right == NULL))
     {
         node->parent = NULL;
         free(node);
